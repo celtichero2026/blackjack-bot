ca0eb8981faa6fd573c16ed81e8f9bf9eddece06
celtichero2026 <celtichero2026@gmail.com>
2026-05-29 00:02:05 -0500

    Add Blackjack cog with command to create a table

--- cogs/blackjack.py
+++ cogs/blackjack.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
 
+
+class Blackjack(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="blackjack", description="Create a blackjack table")
+    async def blackjack(self, interaction: discord.Interaction):
+        await interaction.response.send_message("🎰 Blackjack table created!")
+
+
+async def setup(bot):
+    await bot.add_cog(Blackjack(bot))
